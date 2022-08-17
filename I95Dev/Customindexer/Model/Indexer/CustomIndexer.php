2ab5a689cf7314d814b4aa14e500e8ba8c061c58
sravanth007 <90679731+sravanth007@users.noreply.github.com>
2022-08-17 09:39:10 +0530

    Add files via upload

---
+++ I95Dev/Customindexer/Model/Indexer/CustomIndexer.php
@@ -0,0 +1,17 @@
+<?php
+namespace I95Dev\Customindexer\Model\Indexer;
+
+class CustomIndexer implements \Magento\Framework\Indexer\ActionInterface, \Magento\Framework\Mview\ActionInterface
+
+{
+	public function executeFull(){}
+	 //Should take into account all placed orders in the system
+
+	public function executeList($ids){}
+	 //Works with a set of placed orders (mass actions and so on)
+
+	public function executeRow($id){}
+	 //Works in runtime for a single order using plugins
+
+	public function execute($ids){} //Used by mview, allows you to process multiple placed orders in the "Update on schedule" mode 	
+}
---
+++ I95Dev/Customindexer/registration.php
@@ -0,0 +1,7 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'I95Dev_Customindexer',
+    __DIR__
+);
